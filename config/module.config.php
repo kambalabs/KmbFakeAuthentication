157aa182a9d7c4bb5f17fe0822b0e83689ade7dd
Emmanuel Bouton <goten4@gmail.com>
2014-08-04 16:51:11 +0200

    Add fake route signout

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,17 @@
 <?php
 return [
+    'router' => [
+        'routes' => [
+            'signout' => [
+                'type' => 'Literal',
+                'options' => [
+                    'route' => '/signout',
+                    'defaults' => [
+                    ],
+                ],
+            ],
+        ],
+    ],
     'service_manager' => [
         'invokables' => [
             'KmbFakeAuthentication\Adapter\Fake' => 'KmbFakeAuthentication\Adapter\Fake',
